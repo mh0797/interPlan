a85024a48935c92ec0d7c2a8cc2e583d01514469
Marcel Hallgarten <marcel.hallgarten@de.bosch.com>
2024-08-29 11:57:21 +0200

    Fix typo

--- scripts/sim_pdm_closed.sh
+++ scripts/sim_pdm_closed.sh
@@ -5,13 +5,14 @@ INTERPLAN_PLUGIN_ROOT=~/interplan_workspace/interplan-plugin
 python $INTERPLAN_PLUGIN_ROOT/interplan/planning/script/run_simulation.py \
 +simulation=default_interplan_benchmark \
 planner=pdm_closed_planner \
+scenario_filter=interplan10 \
 experiment_name=$EXPERIMENT \
 hydra.searchpath="[\
 pkg://interplan.planning.script.config.common,\
 pkg://interplan.planning.script.config.simulation,\
 pkg://interplan.planning.script.experiments,\
-pkg://nuplan_garage.planning.script.config.common,\
-pkg://nuplan_garage.planning.script.config.simulation,\
+pkg://tuplan_garage.planning.script.config.common,\
+pkg://tuplan_garage.planning.script.config.simulation,\
 pkg://nuplan.planning.script.config.common,\
 pkg://nuplan.planning.script.config.simulation,\
 pkg://nuplan.planning.script.experiments\
